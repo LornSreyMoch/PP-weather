66847e8fdaddf081b8570ff43a08dc77e6d0fea6
MochNoch <sreymoch.lorn@institute.pse.ngo>
2024-05-07 06:59:58 +0700

    change page

--- pages/react/index.js
+++ pages/react/index.js
@@ -28,3 +28,34 @@ function remove() {
 		i++;
 	}
 }
+
+const formEl = document.querySelector('.form');
+
+formEl.addEventListener('Submit', event => {
+    event.preventDefault();
+
+    const formData = new FormData(formEl);
+    const data = Object.fromEntries(formData);
+    
+    fetch('https://weather-backend-kappa.vercel.app/reactions',{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'api-key': '321dd35cfdb912208ad17ff541fa5335170e957ef19362431897d25599b703d0'
+        },
+        body: JSON.stringify(data)
+    }).then(res => res.json())
+      .then(data => console.log(data))
+      .catch(error => console.log(error));
+});
+
+
+fetch('https://weather-backend-kappa.vercel.app/reactions',{
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'api-key': '321dd35cfdb912208ad17ff541fa5335170e957ef19362431897d25599b703d0'
+        },
+    }).then(res => res.json())
+      .then(data => console.log(data))
+      .catch(error => console.log(error));
